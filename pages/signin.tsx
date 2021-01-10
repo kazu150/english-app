17e95c286e35395fb383ed5e4233bf2cf91cf072
Kazushi Isogai <kazu150@gmail.com>
2021-01-11 01:09:56 +0900

    siginupページのreducerつなぎこみ

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,7 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useContext } from 'react';
+import { MyContext } from './_app';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+
+type SignInUser = {
+    email: string;
+    password: string;
+};
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,16 +23,54 @@ const useStyles = makeStyles((theme) => ({
 
 const SignIn: FC = () => {
     const classes = useStyles();
+    const [signInUser, setSignInUser] = useState<SignInUser>({
+        email: '',
+        password: '',
+    });
+    const { dispatch } = useContext(MyContext);
+
+    const onSignInButtonClick = (e) => {
+        e.preventDefault();
+        console.log(signInUser);
+        dispatch({
+            type: 'user_signup',
+            payload: signInUser,
+        });
+    };
     return (
         <form className={classes.root} noValidate autoComplete="off">
-            <TextField fullWidth id="standard-basic" label="メールアドレス" />
+            <TextField
+                fullWidth
+                id="standard-basic"
+                label="メールアドレス"
+                value={signInUser.email}
+                onChange={(e) =>
+                    setSignInUser({
+                        ...signInUser,
+                        email: e.target.value,
+                    })
+                }
+            />
             <TextField
                 fullWidth
                 id="standard-basic"
                 label="パスワード"
                 type="password"
+                value={signInUser.password}
+                onChange={(e) =>
+                    setSignInUser({
+                        ...signInUser,
+                        password: e.target.value,
+                    })
+                }
             />
-            <Button variant="contained">ログイン</Button>
+            <Button
+                variant="contained"
+                type="submit"
+                onClick={onSignInButtonClick}
+            >
+                ログイン
+            </Button>
         </form>
     );
 };
