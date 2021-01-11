b4734f0f55d392cc083bba680c5734f9f3830544
Kazushi Isogai <kazu150@gmail.com>
2021-01-11 17:43:10 +0900

    signUpページのreducer作成

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useContext } from 'react';
+import { MyContext } from './_app';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -17,24 +18,63 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const SignUp: FC = () => {
+    const { dispatch, state } = useContext(MyContext);
+
     const classes = useStyles();
+    const [signUpUser, setSignUpUser] = useState({
+        mail: '',
+        password: '',
+        passwordConfirm: '',
+    });
+
+    const onSignUpSubmit = () => {
+        dispatch({ type: 'user_signup', payload: signUpUser });
+    };
+
     return (
         <form className={classes.root} noValidate autoComplete="off">
-            <TextField fullWidth id="standard-basic" label="メールアドレス" />
+            <TextField
+                fullWidth
+                id="standard-basic"
+                label="メールアドレス"
+                value={signUpUser.mail}
+                onChange={(e) =>
+                    setSignUpUser({
+                        ...signUpUser,
+                        mail: e.target.value,
+                    })
+                }
+            />
             <TextField
                 fullWidth
                 id="standard-basic"
                 label="パスワード"
                 type="password"
+                value={signUpUser.password}
+                onChange={(e) =>
+                    setSignUpUser({
+                        ...signUpUser,
+                        password: e.target.value,
+                    })
+                }
             />
             <TextField
                 fullWidth
                 id="standard-basic"
                 label="パスワード(確認用)"
                 type="password"
+                value={signUpUser.passwordConfirm}
+                onChange={(e) =>
+                    setSignUpUser({
+                        ...signUpUser,
+                        passwordConfirm: e.target.value,
+                    })
+                }
             />
             <Link href="./">
-                <Button variant="contained">会員登録</Button>
+                <Button onClick={onSignUpSubmit} variant="contained">
+                    会員登録
+                </Button>
             </Link>
         </form>
     );
