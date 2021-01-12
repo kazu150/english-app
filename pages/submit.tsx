a48e71178d509202438cc3aa7bdc5118d3e422b9
Kazushi Isogai <kazu150@gmail.com>
2021-01-12 11:23:19 +0900

    submit.tsxの間違い修正

--- pages/submit.tsx
+++ pages/submit.tsx
@@ -18,11 +18,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Submit = () => {
-    const { dispatch } = useContext(MyContext);
+    const { state, dispatch } = useContext(MyContext);
     const classes = useStyles();
     const [result, setResult] = useState({
         service: '',
-        count: '',
+        count: null,
         nationality: '',
     });
 
@@ -35,37 +35,39 @@ const Submit = () => {
     return (
         <div>
             <h2>英会話をやりました！</h2>
-            <InputLabel id="demo-simple-select-label">利用サービス</InputLabel>
+            <InputLabel id="service">利用サービス</InputLabel>
             <Select
                 fullWidth
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
+                labelId="service"
+                id="service"
                 value={result.service}
             >
-                <MenuItem value={10}>DMM英会話</MenuItem>
-                <MenuItem value={20}>レアジョブ</MenuItem>
-                <MenuItem value={30}>ネイティブキャンプ</MenuItem>
+                <MenuItem value={'DMM英会話'}>DMM英会話</MenuItem>
+                <MenuItem value={'レアジョブ'}>レアジョブ</MenuItem>
+                <MenuItem value={'ネイティブキャンプ'}>
+                    ネイティブキャンプ
+                </MenuItem>
             </Select>
             <p>一回の英会話時間：２５分</p>
             <FormControl component="fieldset">
                 <FormLabel component="legend">実施回数</FormLabel>
                 <RadioGroup
-                    aria-label="gender"
-                    name="gender1"
+                    aria-label="count"
+                    name="count1"
                     value={result.count}
                 >
                     <FormControlLabel
-                        value="female"
+                        value={1}
                         control={<Radio />}
                         label="１回"
                     />
                     <FormControlLabel
-                        value="male"
+                        value={2}
                         control={<Radio />}
                         label="２回"
                     />
                     <FormControlLabel
-                        value="other"
+                        value={3}
                         control={<Radio />}
                         label="３回"
                     />
@@ -81,12 +83,18 @@ const Submit = () => {
                 id="demo-simple-select"
                 value={result.nationality}
             >
-                <MenuItem value={10}>アメリカ</MenuItem>
-                <MenuItem value={20}>イギリス</MenuItem>
-                <MenuItem value={30}>オーストラリア</MenuItem>
-                <MenuItem value={30}>その他</MenuItem>
+                <MenuItem value="US">アメリカ</MenuItem>
+                <MenuItem value="UK">イギリス</MenuItem>
+                <MenuItem value="AUS">オーストラリア</MenuItem>
+                <MenuItem value="OTHERS">その他</MenuItem>
             </Select>
-            <p>合計：XX分</p>
+            <p>
+                合計：
+                {state.services.filter(
+                    (service) => service.name === result.service
+                )[0].timePerLesson * result.count}
+                分
+            </p>
             <Button
                 className={classes.button}
                 fullWidth
