35547ce00124a01843a88df446858cb95df3c9b8
Kazushi Isogai <kazu150@gmail.com>
2021-01-21 14:02:55 +0900

    cloudFunctions内の非同期処理にtry/catchを追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,20 +14,26 @@ export const sumUpStudyTimeOnChangeInitialTime = functions
             const newInitialTime = Number(change.after.data().initialTime);
             const currentInitialTime = Number(change.before.data().initialTime);
 
-            const publicProfile = await db
-                .collection('publicProfiles')
-                .doc(context.params.uid)
-                .get();
-            const currentStudyTime =
-                Number(publicProfile.data()?.studyTime) || 0;
+            try {
+                const publicProfile = await db
+                    .collection('publicProfiles')
+                    .doc(context.params.uid)
+                    .get();
+                const currentStudyTime =
+                    Number(publicProfile.data()?.studyTime) || 0;
 
-            await db
-                .collection('publicProfiles')
-                .doc(context.params.uid)
-                .update({
-                    studyTime:
-                        currentStudyTime - currentInitialTime + newInitialTime,
-                });
+                await db
+                    .collection('publicProfiles')
+                    .doc(context.params.uid)
+                    .update({
+                        studyTime:
+                            currentStudyTime -
+                            currentInitialTime +
+                            newInitialTime,
+                    });
+            } catch (error) {
+                console.log('エラー内容：', error);
+            }
         }
     });
 
@@ -35,26 +41,33 @@ export const sumUpStudyTimeOnWriteStudyLog = functions
     .region('asia-northeast1')
     .firestore.document('users/{uid}/studyLog/{id}')
     .onWrite(async (change, context) => {
-        const studyLogs = await db
-            .collection('users')
-            .doc(context.params.uid)
-            .collection('studyLog')
-            .where('time', '>=', 0)
-            .get();
+        try {
+            const studyLogs = await db
+                .collection('users')
+                .doc(context.params.uid)
+                .collection('studyLog')
+                .where('time', '>=', 0)
+                .get();
 
-        let totalLogs = 0;
-        studyLogs.forEach((doc) => {
-            totalLogs += doc.data().time;
-        });
-
-        const user = await db.collection('users').doc(context.params.uid).get();
-
-        const currentInitialTime = Number(user.data()?.initialTime) || 0;
-
-        await db
-            .collection('publicProfiles')
-            .doc(context.params.uid)
-            .update({
-                studyTime: totalLogs + currentInitialTime,
+            let totalLogs = 0;
+            studyLogs.forEach((doc) => {
+                totalLogs += doc.data().time;
             });
+
+            const user = await db
+                .collection('users')
+                .doc(context.params.uid)
+                .get();
+
+            const currentInitialTime = Number(user.data()?.initialTime) || 0;
+
+            await db
+                .collection('publicProfiles')
+                .doc(context.params.uid)
+                .update({
+                    studyTime: totalLogs + currentInitialTime,
+                });
+        } catch (error) {
+            console.log('エラー内容：', error);
+        }
     });
